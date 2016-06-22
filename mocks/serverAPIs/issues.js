984516d2b29478fb0a258d72f5e0c3e222a1280d
abhay-joshi <abhay.joshi@drillinginfo.com>
2016-06-22 19:53:11 +0530

    changes done to load dynamic data

--- mocks/serverAPIs/issues.js
+++ mocks/serverAPIs/issues.js
@@ -1,5 +1,43 @@
 import issuesData from '../data/issues.json';
 
+const issues = [...issuesData.issues];
+const originalDataLen = issuesData.issues.length;
+
 export const loadIssues = (offset, qty) => {
-    return issuesData.issues.slice(offset, offset + qty);
+    let totalQtyReq = offset + qty;
+    let totalQtyAvailable = issues.length;
+
+    if (totalQtyReq <= totalQtyAvailable) {
+        return issues.slice(offset, offset + qty);
+    }
+
+    let numIssuesToBeAdded = totalQtyReq - totalQtyAvailable;
+
+    let numOfTimes = Math.floor(numIssuesToBeAdded / originalDataLen);
+    let remainder = numIssuesToBeAdded % originalDataLen;
+    let newData = [];
+
+    for (let i = 0; i < numOfTimes; i ++) {
+        newData = [...newData, ...issuesData.issues]
+    }
+
+    newData = [...newData, ...issuesData.issues.slice(0, remainder)];
+
+    let availableStories = issues.filter(item => { return item.type === 'story' });
+    let availableBugs = issues.filter(item => { return item.type === 'bug' });
+
+    let storyCount = availableStories.length;
+    let bugCount = availableBugs.length;
+
+    newData = newData.map((item, index) => {
+        let currItem = {...item};
+        if (item.type === 'story') {
+            currItem.id = 'S' + ++storyCount;
+        } else if (item.type === 'bug') {
+            currItem.id = 'B' + ++bugCount
+        }
+        return currItem;
+    });
+
+    return newData.slice(newData.length - qty)
 }
